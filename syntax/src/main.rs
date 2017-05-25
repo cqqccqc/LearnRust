c4122bd7639010780ed8451f8707d486a06dbfd9
陈琦 <cqqccqc@foxmail.com>
2017-05-25 13:53:47 +0800

    build file

--- syntax/src/main.rs
+++ syntax/src/main.rs
@@ -1,25 +1,4 @@
 fn main() {
-    println!("Hello, world!");
-
-    let i = 1;
-    let i2 = i;
-
-    println!("iii = {}", i);
-
-    let v = vec![1, 2, 3];
-    let v = take(v);
-    // let mut v2 = v;
-
-    // v2.truncate(2);
-
-    println!("v[0] is {}", v[0]);
-    
-}
-fn take(v: Vec<i32>) -> Vec<i32> {
-    // println!("v[0] is {}", v[0]);
-    v
-}
-
-fn foo(v1: Vec<i32>, v2: Vec<i32>) -> (Vec<i32>, Vec<i32>) {
-    unimplemented!();
+    let mut x = 5;
+    x = 10;
 }
