98081bba0fcdbde6749c4d59e0d033f0fc0ae613
q.chen <q.chen@ctrip.com>
2018-11-23 18:54:37 +0800

    rrr

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,15 +1,33 @@
 fn main() {
-    // println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 32,
+        height: 50,
+    };
 
-    let width1 = 30;
-    let height1 = 50;
-
+    println!("rect1 is {:#?}", rect1);
     println!(
-        "The area of rectangle is {} square pixels",
-        area(width1, height1)
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    return width * height;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
